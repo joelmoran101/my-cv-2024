748870a48d4d75d735050fc454a66033ed48b83b
Joel.M <joel.moran@students.beaminstitute.org>
2024-10-07 10:05:20 +0200

    jm:made minor styles improvement to make the toogle button for the language to be more apparent.

--- components/LanguageToggle.tsx
+++ components/LanguageToggle.tsx
@@ -9,7 +9,8 @@ export const LanguageToggle: React.FC = () => {
   return (
     <Button
       onClick={() => setLanguage(language === 'en' ? 'de' : 'en')}
-      className="fixed top-4 right-4 z-10"
+      // className="fixed top-4 right-4 z-10"
+      className="bg-accent-dark py-0 px-3 fixed top-5 right-5 z-10 "
     >
       {language === 'en' ? 'Deutsch' : 'English'}
     </Button>
